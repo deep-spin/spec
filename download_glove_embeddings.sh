23d2e9e9f4b5e94f620c8168472451f28244679a
Marcos Treviso <marcosvtreviso@gmail.com>
2021-01-18 09:18:49 -0400

    Update download glove embs script and add gensim to requirements

--- download_glove_embeddings.sh
+++ download_glove_embeddings.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p data/embs
-cd data/embs || exit
+mkdir -p data/embs/glove
+cd data/embs/glove || exit
+
 wget -c https://allennlp.s3.amazonaws.com/datasets/glove/glove.840B.300d.txt.gz
 gzip -d glove.840B.300d.txt.gz
 rm glove.840B.300d.txt.gz
