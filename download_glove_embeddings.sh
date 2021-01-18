49db8e988ef1a79d8d8500abf3bab66129345618
Marcos Treviso <marcosvtreviso@gmail.com>
2021-01-18 07:32:14 -0400

    Fix broken path

--- download_glove_embeddings.sh
+++ download_glove_embeddings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p data/embs
-cd daat/embs || exit
+cd data/embs || exit
 wget -c https://allennlp.s3.amazonaws.com/datasets/glove/glove.840B.300d.txt.gz
 gzip -d glove.840B.300d.txt.gz
 rm glove.840B.300d.txt.gz
